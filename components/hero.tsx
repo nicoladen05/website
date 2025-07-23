7e6508d5970f4488973699f9517558cff59639d2
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-07-24 00:57:32 +0200

    fix the padding

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,7 +2,7 @@ import { Github, Mail } from "lucide-react"
 
 export default function Hero() {
   return (
-    <section className="container flex min-h-screen max-w-screen-2xl flex-col items-center justify-center space-y-12 py-24 text-center">
+    <section className="container flex min-h-screen max-w-screen-2xl flex-col items-center justify-center py-24 text-center">
       <div className="space-y-6">
         <h1 className="bg-gradient-to-br from-foreground from-30% via-foreground/90 to-foreground/70 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl md:text-6xl lg:text-7xl">
           nicoladen
